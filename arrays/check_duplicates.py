9aa12bf91179715b30ee80ba20161f3ce98a0b48
Abiney01 <abineynew@gmail.com>
2025-12-26 22:06:44 +0530

    updated files

--- arrays/check_duplicates.py
+++ arrays/check_duplicates.py
@@ -1,8 +1,17 @@
 def check_duplicate(arr):
+    # for i in arr:
+    #     if arr.count(i) > 0 :
+    #         return True
+    # return False
+
+    # optimal O(n) and O(n) -> Time and Space
+    hashset = set()
     for i in arr:
-        if arr.count(i) > 0 :
+        if i in hashset:
             return True
+        hashset.add(i)
     return False
+
 
 arr = [1,2,3,4]
 print(check_duplicate(arr))
--- strings/anagrams.py
+++ strings/anagrams.py
@@ -1,9 +1,21 @@
 def anagrams(s1,s2):
-    a = sorted(s1)
-    b = sorted(s2)
-    if a==b:
-        return 1
-    return 0
+    # a = sorted(s1)
+    # b = sorted(s2)
+    # if a==b:
+    #     return 1
+    # return 0
+
+    # optimal O(n) O(1) -> time and space 
+    if len(s1)!=len(s2):
+         return False
+    count = [0]*26
+    for i in range(len(s1)):
+        count[ord(s1[i]) - ord('a')]+=1
+        count[ord(s2[i]) - ord('a')]-=1
+    for val in count:
+        if val!=0:
+            return False
+        return True
 s1 = "listen"
 s2 = "silent"
 print(anagrams(s1,s2))
