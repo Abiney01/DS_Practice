ad8f9ce8129fb37bc014833536d5a0bbfdd999f9
Abiney01 <abineynew@gmail.com>
2026-03-09 10:37:17 +0530

    added file

---
+++ intervals/insert_interval.py
@@ -0,0 +1,16 @@
+def insert_interval(intervals,newInterval):
+    res = []
+    for i in range(len(intervals)):
+        if newInterval[1] < intervals[i][0]:
+            res.append(newInterval)
+            return res + intervals[i:]
+        elif newInterval[0] > intervals[i][1]:
+            res.append(intervals[i])
+        else:
+            newInterval = [min(newInterval[0],intervals[i][0]),max(newInterval[1],intervals[i][1])]
+    res.append(newInterval)
+    return res
+
+intervals = [[1,3],[4,6]]
+newInterval = [2,5]
+print(insert_interval(intervals,newInterval))
