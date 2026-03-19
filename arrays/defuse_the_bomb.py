c6e86edb425deefa9b9a04a5e792a47c116cd407
Abiney01 <abineynew@gmail.com>
2026-03-19 20:01:51 +0530

    added file

---
+++ arrays/defuse_the_bomb.py
@@ -0,0 +1,21 @@
+def defuseBomb(code,k):
+    n = len(code)
+    res = [0] * n
+    l = 0
+    cur_sum = 0
+    for r in range(n+abs(k)):
+        cur_sum += code[r%n]
+        if r-l+1 > abs(k):
+            cur_sum -= code[l%n]
+            l = (l+1)%n
+        
+        if  r-l+1 == abs(k):
+            if k > 0:
+                res[(l-1)%n] = cur_sum
+            elif k < 0:
+                res[(r+1)%n] = cur_sum
+    return res
+
+code = [5,7,1,4]
+k = 3
+print(defuseBomb(code,k))
